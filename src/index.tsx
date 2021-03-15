14e58c159d4b0e49c343c7b396b1271790f60156
Lyubov Myetolkina <luba150278@gmail.com>
2021-03-15 13:22:53 +0200

    lesson 1.9 first 6min

--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,27 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './components/app/app';
+// import App from './components/app/app';
+
+interface UserData {
+  name: string;
+  surname: string;
+  link: string;
+}
+const userData: UserData = { name: 'John', surname: 'Ivanov', link: 'facebook.com' };
+
+const WhoAmI: React.FC<UserData> = ({ name, surname, link }) => (
+  <>
+    <h1>
+      My name is {name}, surname - {surname}
+    </h1>
+    <a href={link}>{link}</a>
+  </>
+);
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <WhoAmI name={userData.name} surname={userData.surname} link={userData.link} />
   </React.StrictMode>,
   document.getElementById('root')
 );
